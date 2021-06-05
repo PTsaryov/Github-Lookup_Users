7e1d884a9ab4ee1bc4ef0b31bb71675fbb50f456
PTsaryov <pavtsaryov@gmail.com>
2021-06-05 14:20:58 -0600

    2 files modified

--- index.js
+++ index.js
@@ -3,32 +3,29 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
+const searchBox = document.getElementById('searcinput');
+
+
 
 function getUsers() {
   fetch(
-    'https://api.github.com/search/users?state:open&order=asc&q=' + userValue,
+    'https://api.github.com/search/users?state:open&order=asc&q=' + searchBox.value,
     {
       headers: { Accept: 'application/vnd.github.v3+json' }
     }
   ).then(response =>
-    response
-      .json()
-      .then(parsedJSON =>
-        parsedJSON.items.map(user => ({
-          name: `${user.login}`,
-          thumbnail: `${user.avatar_url}`,
-          page_url: `${user.html_url}`
-        }))
+    response.json()
+      .then(data => {
+        const userData = data.items.map(user => ({
+          Name: `${user.login}`,
+          Avatar: `${user.avatar_url}`,
+          Page_Url: `${user.html_url}`
+        }))}
       )
-      .then(users =>
-        this.setState({
-          users
-        })
+      .then(userData => {
+        app.innerHTML = ``;
+
+      }
       )
-      .catch(error => this.error_message(error))
   );
 }
-
-appDiv.innerHTML = `<h1>Search Github Users</h1>
-                    <input ></input>
-                    <button>Search</button>`;
