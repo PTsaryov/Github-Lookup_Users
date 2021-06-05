48dbc952d49eb3bd26c9a5259ec75217563aefc7
PTsaryov <pavtsaryov@gmail.com>
2021-06-05 16:10:48 -0600

    4 files modified

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# js-ccjjjx
+# GitHub User Search Exercise
+ 
+This is an exercise for re-implementing a postion of GitHub's search feature, the user search, using their public API.
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ccjjjx)
+
+Below is a list of resources I used to help me along as it in my first time writing in Javascript:
+
+https://www.howtosolutions.net/2016/05/visual-studio-creating-javascript-project/
+https://wesbos.com/javascript/04-the-dom/creating-html
+https://docs.github.com/en/rest/reference/search
+https://www.loginradius.com/blog/async/github-api/
+https://www.pluralsight.com/guides/using-fetch-with-github-api-v3
--- index.js
+++ index.js
@@ -3,29 +3,31 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-const searchBox = document.getElementById('searcinput');
+const searchBox = document.getElementById('searchinput');
 
-
+document.getElementById('searchbutton').addEventListener('mousedown', getUsers);
 
 function getUsers() {
   fetch(
-    'https://api.github.com/search/users?state:open&order=asc&q=' + searchBox.value,
+    'https://api.github.com/search/users?state:open&order=asc&q=' +
+      searchBox.value,
     {
       headers: { Accept: 'application/vnd.github.v3+json' }
     }
   ).then(response =>
-    response.json()
+    response
+      .json()
       .then(data => {
+        //console.log(searchBox.value);  //uncomment to see that value is being grabbed from the search box
         const userData = data.items.map(user => ({
           Name: `${user.login}`,
           Avatar: `${user.avatar_url}`,
           Page_Url: `${user.html_url}`
-        }))}
-      )
+        }));
+        //console.log(userData); //uncomment to see that the array is being populated with objects (users)
+      })
       .then(userData => {
-        app.innerHTML = ``;
-
-      }
-      )
+        appDiv.innerHTML = data.ToString();
+      })
   );
 }
